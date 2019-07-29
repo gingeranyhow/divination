bc17b7cdb967c2632d79526d2e320f1cd19dd9cb
Patrick Ewing <patrick@chanceagency.com>
2019-07-28 17:59:12 -0700

    Package our iChingHelper as a node_module... but why won't require() find it?

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const nunjucks = require('nunjucks');
 const express = require('express');
 const fileUpload = require('express-fileupload');
 
+const iChingHelper = require('iChingHelper');
+
 var stream = require('stream');
 var Busboy = require('busboy');
 
@@ -27,13 +29,6 @@ nunjucks.configure('views', {
     express: app
 });
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-// app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   var title = "I Ching Passwords "
   var hexagram = iChing.hexagram(getRandomInt(64));
@@ -50,14 +45,12 @@ app.get('/upload', function(request, response) {
   response.render('upload.html', {bytes: readBytes("/app/public/uploads/Text File.txt")});
 });
 
-
 app.post('/upload', function(req, res) {
     var busboy = new Busboy({ headers: req.headers });
     var saveTo = "";
     busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
       saveTo = path.join('./public/uploads', filename);
       console.log('Uploading to: ' + saveTo);
-      // debugger;
       file.pipe(fs.createWriteStream(saveTo));
     });
     busboy.on('finish', function() {
@@ -69,36 +62,6 @@ app.post('/upload', function(req, res) {
     return req.pipe(busboy);
 });
 
-function readBytes(filePath) {
-  var myStream = fs.createReadStream(filePath, {
-    // objectMode: true,
-    // highWaterMark: 1,
-  }).pause().setEncoding('hex');;
-  
-  console.log("Reading bytes");
-  var output = "nooooo";
-  var chonk = 0;
-  
-  while (myStream.readable) {
-    var chunk = myStream.read();
-    console.log(chonk)
-    console.log(chunk);
-    chonk ++;
-  }
-  
-  // myStream.on("data", function(data) {
-  //     var chunk = data.toString();
-  //     // output = `Received ${chunk.length} bytes of data.`
-  //     output = "supppp";
-  //     console.log(output);
-  //     // if (chunk.length > 0) {
-  //     //     output = chunk;
-  //     // }
-  //     console.log(chunk);
-  // });
-  console.log("outer", output);
-  return output;
-}
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
